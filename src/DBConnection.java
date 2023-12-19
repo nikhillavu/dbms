cd9d433b92c64ed8dc7f2881640b1933c01f81cf
NikhilLavu <lavunikhilkrishna@gmail.com>
2023-12-19 13:41:30 -0500

    Added My Files

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Koundi@123";
+    private static final String PASSWORD = "Nikhil@123";
     
     public static java.sql.Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
